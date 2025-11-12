c2f59b4b416c96c8200d36cfbe9f5e7c5551cd04
Félix Saparelli <felix@bes.au>
2025-11-12 22:04:26 +1300

    prefer partuuid for grub

--- iso/bootloader.sh
+++ iso/bootloader.sh
@@ -3,6 +3,8 @@ set -ex
 
 sed -i 's/GRUB_TIMEOUT=.+/GRUB_TIMEOUT=5/' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT_STYLE=.+/GRUB_TIMEOUT_STYLE=menu/' /etc/default/grub
+echo GRUB_DISABLE_LINUX_UUID=true >> /etc/default/grub
+echo GRUB_DISABLE_LINUX_PARTUUID=false >> /etc/default/grub
 
 update-initramfs -u -k all
 update-grub
