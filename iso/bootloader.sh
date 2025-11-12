7c271fa07a1b25ef4127093d724c7c893eaf9967
Félix Saparelli <felix@bes.au>
2025-11-13 01:40:53 +1300

    use dracut

--- iso/bootloader.sh
+++ iso/bootloader.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+apt install -y dracut # removes initramfs-tools
+dracut -H --hostonly-mode=sloppy --force
+
 sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=5/' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="noresume"/' /etc/default/grub
@@ -9,6 +12,5 @@ echo GRUB_DISABLE_LINUX_PARTUUID=false >> /etc/default/grub
 
 rm -rf /boot/grub || true
 mkdir /boot/grub
-update-initramfs -u -k all
 update-grub
 grub-install --bootloader-id=ubuntu
