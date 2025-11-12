1075c02eff013a42385631b7dedba2999f59f906
Félix Saparelli <felix@bes.au>
2025-11-13 01:41:07 +1300

    force partuuid for grub

--- iso/bootloader.sh
+++ iso/bootloader.sh
@@ -7,8 +7,9 @@ dracut -H --hostonly-mode=sloppy --force
 sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=5/' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="noresume"/' /etc/default/grub
-echo GRUB_DISABLE_LINUX_UUID=true >> /etc/default/grub
-echo GRUB_DISABLE_LINUX_PARTUUID=false >> /etc/default/grub
+
+ROOT_PART="/dev/$(lsblk -ln -o NAME,PARTLABEL | grep 'root' | awk '{print $1}')"
+echo GRUB_FORCE_PARTUUID="$(blkid -s PARTUUID -o value $ROOT_PART)" >> /etc/default/grub
 
 rm -rf /boot/grub || true
 mkdir /boot/grub
